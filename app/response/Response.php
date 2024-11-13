8aaa1bda83f31f952eea7bf081c38454ace98f42
rkmud <korbutustina@icloud.com>
2024-11-13 15:21:33 +0300

    update response

--- app/response/Response.php
+++ app/response/Response.php
@@ -8,14 +8,17 @@ class Response
 {
     public function view(string $view, array $data = [], int $status = 200): void
     {
+        http_response_code($status);
+
         $views_path = sprintf('app/views/%sView.php', $view);
 
         if (!file_exists($views_path)) {
             echo 'Not found';
+
             return;
         }
 
-        if ($data != []) {
+        if (!empty($data)) {
             extract($data, EXTR_PREFIX_SAME, 'data_');
         }
 
